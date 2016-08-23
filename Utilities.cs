65571a8005be635c2d3be6e43a0d3669d93a28b2
Berkley Bernales <Berkley@Berks-MacBook-Pro.local>
2016-08-23 20:22:38 +0800

    Initial commit

---
+++ Utilities.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+
+namespace Motion.Mobile.Utilities
+{
+	public static class Utilities
+	{
+		public static string StringToHexString(string param)
+		{
+			char[] charValues = param.ToCharArray();
+			string hexOutput="";
+			foreach (char _eachChar in charValues )
+			{
+				// Get the integral value of the character.
+				int value = Convert.ToInt32(_eachChar);
+				// Convert the decimal value to a hexadecimal value in string form.
+				hexOutput += String.Format("{0:X}", value);
+				// to make output as your eg 
+				//  hexOutput +=" "+ String.Format("{0:X}", value);
+			}
+			return hexOutput;
+		}
+
+		public static byte[] StringToByteArray(string str) 
+		{
+			byte[] bytes = new byte[str.Length * sizeof(char)];
+			System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+			return bytes;
+		}
+
+		public static string GetString(byte[] bytes)
+		{
+			char[] chars = new char[bytes.Length / sizeof(char)];
+			System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+			return new string(chars);
+		}
+
+		public static string getMonthFullName(DateTime date)
+		{
+			return CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName (date.Month);
+		}
+
+		public static string getMonthShortName(DateTime date)
+		{
+			return CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName (date.Month);
+		}
+
+
+
+	}
+}
+
